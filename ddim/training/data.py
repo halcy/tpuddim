25f350f8efffd337a2efe7bd448e837d0031ca61
Lorenz Diener <lorenzd@gmail.com>
2021-08-25 18:43:35 +0000

    improve steps and dataloading

--- ddim/training/data.py
+++ ddim/training/data.py
@@ -1,5 +1,10 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
+import glob
+import PIL
+
+from functools import partial
 
 class TimestepSampler():
     """
@@ -28,10 +33,45 @@ class DataSampler():
     def __init__(self, base_data, batch_size):
         self.base_data = base_data
         self.batch_size = batch_size
-        pass
     
     def sample(self, prng):
         prng, key = jax.random.split(prng)
         indices_sample = jax.random.choice(key, len(self.base_data), (self.batch_size,))
         sample = self.base_data[indices_sample]
         return prng, sample
+
+class ImageDataset():
+    """
+    Basic image dataset. TODO: Augmentations, caching
+    """
+    def __init__(self, file_glob, resize = None):
+        self.file_list = list(sorted(list(glob.glob(file_glob))))
+        self.resize = resize
+        
+    def __len__(self):
+        return len(self.file_list)
+    
+    def __getitem__(self, i):
+        image = PIL.Image.open(self.file_list[i])
+        if not self.resize is None:
+            image.thumbnail((self.resize, self.resize), PIL.Image.ANTIALIAS)
+        return jnp.array(image)    
+
+class DatasetSampler():
+    """
+    Very basic data sampler that returns randomly picked batches from a Dataset class
+    """
+    def __init__(self, dataset, batch_size, norm_fact = 127.5, norm_sub = 1.0):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shape = tuple([batch_size] + list(dataset[0].shape))
+        self.norm_fact = norm_fact
+        self.norm_sub = norm_sub
+        self.sample_arr = np.zeros(self.shape)
+    
+    def sample(self, prng):
+        prng, key = jax.random.split(prng)
+        indices_sample = jax.random.choice(key, len(self.dataset), (self.batch_size,))
+        for i, index in enumerate(indices_sample):
+            self.sample_arr[i] = self.dataset[index]
+        return prng, (jnp.array(self.sample_arr) / self.norm_fact) - self.norm_sub
--- ddim/training/steps.py
+++ ddim/training/steps.py
@@ -1,5 +1,6 @@
 import optax
 import jax
+import jax.numpy as jnp
 from jax.experimental.pjit import pjit, PartitionSpec
 
 from ..diffusion.loss import loss_fn_mean
@@ -15,7 +16,7 @@ def train_step_update(opt, model, diff_params, params, opt_params, batch, timest
     params = optax.apply_updates(params, updates)
     return loss, params, opt_params
 
-def get_pjit_train_step_update(opt, model, diff_params):
+def get_pjit_train_step_update(opt, model, diff_params, use_pjit = True, donate = True):
     """
     Return a version of train_step_update with opt, model and diff_params curried out and pjit applied
     """
@@ -30,12 +31,18 @@ def get_pjit_train_step_update(opt, model, diff_params):
             timesteps,
             noise
         )
-
-    step_func = pjit(
-        step_func, 
-        [None, None, PartitionSpec("batch", "x", "y"), PartitionSpec("batch"), PartitionSpec("batch", "x", "y")], 
-        [None, None, None]
-    )
+    
+    if use_pjit:
+        donate_argnums = tuple()
+        if donate:
+            donate_argnums = (1, 2, 3, 4)
+            
+        step_func = pjit(
+            step_func, 
+            [None, None, PartitionSpec("batch", "x", "y"), PartitionSpec("batch"), PartitionSpec("batch", "x", "y")], 
+            [None, None, None],
+            donate_argnums = donate_argnums
+        )
     
     return step_func
 
@@ -59,7 +66,7 @@ def train_step(update_func, data_sampler, timestep_sampler, ema, prng, params, o
         params_new = ema.apply(params_new, params)
     return prng, params_new, opt_params_new, loss
 
-def get_pjit_train_step(update_func, data_sampler, timestep_sampler, ema = None):
+def get_pjit_train_step(update_func, data_sampler, timestep_sampler, ema = None, use_pjit = True, donate = True):
     """
     Version of train_step with constant arguments curried out and pjit applied
     """
@@ -73,24 +80,59 @@ def get_pjit_train_step(update_func, data_sampler, timestep_sampler, ema = None)
             params, 
             opt_params
         )
-    return pjit(step_func, None, None)
-
-def get_train_loop(opt, model, diff_params, data_sampler, timestep_sampler, ema, how_many = 1000):
+    if use_pjit:
+        donate_argnums = tuple()
+        if donate:
+            donate_argnums = (0, 1, 2)
+        return pjit(step_func, None, None, donate_argnums = donate_argnums)
+    else:
+        return step_func
+    
+def get_train_loop(opt, model, diff_params, data_sampler, timestep_sampler, ema, how_many = 1000, pjit_loop = True, pjit_update = True, donate = True):
     """
     Get a function that trains for multiple steps (default: 1000) without going back to the host
     """
-    update_func = get_pjit_train_step_update(opt, model, diff_params)
-    step_func = get_pjit_train_step(update_func, data_sampler, timestep_sampler, ema)
+    update_func = get_pjit_train_step_update(opt, model, diff_params, pjit_update, donate)
     
-    def train_iterations(prng, params, opt_params):
+    def train_iterations(prng, params, opt_params, batches):
         def one_step(idx, args):
-            (prng, params, opt_params, loss) = args
-            prng, params, opt_params, loss = step_func(prng, params, opt_params)
-            return (prng, params, opt_params, loss)
+            (prng, params, opt_params, batches, loss) = args
+            
+            # Get data sample
+            batch = batches[idx]
+            
+            # Sample steps, noise
+            prng, timesteps, weights = timestep_sampler.sample(prng)
+            prng, subkey = jax.random.split(prng)
+            noise = jax.random.normal(subkey, batch.shape)
+            
+            # Run update
+            loss, params_new, opt_params_new = update_func(params, opt_params, batch, timesteps, noise)
+            if not ema is None:
+                params_new = ema.apply(params_new, params)
+                
+            return (prng, params_new, opt_params_new, batches, loss)
 
         loss = 0.0
-        args_0 = (prng, params, opt_params, loss)
-        return jax.lax.fori_loop(0, how_many, one_step, args_0)
-
-    return pjit(train_iterations, None, None)
+        args_0 = (prng, params, opt_params, batches, loss)
+        prng, params, opt_params, _, loss = jax.lax.fori_loop(0, how_many, one_step, args_0)
+        return (prng, params, opt_params, loss)
+            
+    if pjit_loop:
+        donate_argnums = tuple()
+        if donate:
+            donate_argnums = (0, 1, 2, 3)
+        train_loop = pjit(train_iterations, None, None, donate_argnums = donate_argnums)
+    else:
+        train_loop = train_iterations
+        
+    def get_data_and_train(prng, params, opt_params):
+        samples = []
+        for i in range(how_many):
+            prng, batch = data_sampler.sample(prng)
+            samples.append(batch)
+        samples = jnp.array(samples)
+        return train_loop(prng, params, opt_params, samples)
+    
+    return get_data_and_train
     
